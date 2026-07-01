d931f5c4987037ae9df95d693c5882940192e323
Anand Mohan Dubey <dubeysatyam95296@gmail.com>
2026-07-01 13:22:37 +0530

    chapter 2

---
+++ CHAPTER 2/02_nagative_string.py
@@ -0,0 +1,16 @@
+name = "aditya_janta_party"
+
+print(name[0:8])
+
+print(name[-11:-3])
+print(name[3:11])
+
+print(name[ :11])#if starting is not menstion than its means its 0
+                   #its same as [0:11]
+
+
+print(name[2 :])#when ending is not mention than its means its last digit
+
+                #its same as [2:18]
+                
+
---
+++ CHAPTER 2/03_string_function.py
@@ -0,0 +1,18 @@
+name = "satyam_dubey"
+
+print(len(name)) #for length of string
+
+print(name.endswith("dubey")) #for find the END letter 
+
+
+print(name.startswith("pan")) #for find the starting letter
+
+print(name.lower()) #convert to small alphabats
+
+print(name.upper()) #convert to capital alphabats
+
+print(name.replace("dubey","pandey")) #for replacing the words from existing words
+
+print(name.find("_")) # for fiding the index from words
+
+
